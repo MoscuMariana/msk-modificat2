be6820fda8b3f71107447d8fd8846970b8fc6c55
MoscuMariana <112778447+MoscuMariana@users.noreply.github.com>
2023-11-05 21:19:26 +0200

    Add files via upload

---
+++ assets/php/nav.php
@@ -0,0 +1,79 @@
+
+<nav id="navbar" class="navbar">
+    <ul>
+      <li>
+        <a class="nav-link scrollto active" href="index.html">Acasă</a>
+      </li>
+      <li>
+        <a class="nav-link scrollto" href="diplome.html">Diplome</a>
+      </li>
+      <li class="dropdown">
+        <a href="#"
+          ><span>Fișe cu cifre</span> <i class="bi bi-chevron-down"></i
+        ></a>
+        <ul>
+          <li>
+            <a href="fise cifre.html">Fișe de activități cu cifre punctate</a>
+          </li>
+          <li>
+            <a href="cifre colorat.html">Cifrele de colorat</a>
+          </li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#"
+          ><span>Fișe cu litere </span>
+          <i class="bi bi-chevron-down"></i
+        ></a>
+        <ul>
+          <li>
+            <a href="fise alfabet.html">Fișe de lipit-Litere de mână</a>
+          </li>
+          <li>
+            <a href="fise alfabet2.html">Fișe de lipit-Litere de tipar</a>
+          </li>
+          <li>
+            <a href="litere de colorat.html"
+              >Literele alfabetului de colorat</a
+            >
+          </li>
+          <li>
+            <a href="gasestelitera.html">Găsește litera</a>
+          </li>
+          <li>
+            <a href="litera-a.html">Litera A- Pachet de resurse</a>
+          </li>
+        </ul>
+      </li>
+      <li class="dropdown megamenu">
+        <a href="#"
+          ><span>Evenimente</span> <i class="bi bi-chevron-down"></i
+        ></a>
+        <ul>
+          <li>
+            <strong>Aprilie</strong>
+            <a href="pastele.html">Paștele </a>
+          </li>
+
+        </ul>
+      </li>
+      </li>
+      <li class="dropdown">
+        <a href="#"
+          ><span>Fișe de colorat</span>
+          <i class="bi bi-chevron-down"></i
+        ></a>
+        <ul>
+          <li>
+            <a href="cuveng.html">Cuvinte în engleză</a>
+          </li>
+          <li>
+            <a href="cifre colorat.html">Cifre de colorat-Monstruleți</a>
+          </li>
+    </ul>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+  <!-- .navbar -->
+
+  
